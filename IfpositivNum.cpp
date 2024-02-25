e32b070c298588ca695209b84986452d298d4fa3
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-25 06:41:26 +0300

    Добавьте файлы проекта.

---
+++ IfpositivNum.cpp
@@ -0,0 +1,23 @@
+﻿// Написать функцию, которая возвращает истину, если передаваемое значение положительное и ложь, если отрицательное
+
+
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+using namespace std;
+
+#define NEGATIVORPOSITIV(num) ((num) > (0))
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int input = 0;
+    while (true) {
+        cout << "Введите значение (для выхода закройте программу): ";        
+        cin >> input;
+      
+        cout << "Введённое значение " << input << " " << (NEGATIVORPOSITIV(input) ? "положительное" : "отрицательное") << endl;        
+    }
+}
